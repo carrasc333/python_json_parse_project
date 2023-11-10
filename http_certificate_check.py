f98f087ca86f7126e92fe199d7fb4c886a2a6f21
Anthony Carrasco <anthony@openpath.com>
2023-11-10 11:57:08 -0800

    script will now detect if percent life remaining is less then 15%

--- http_certificate_check.py
+++ http_certificate_check.py
@@ -2,10 +2,14 @@
 import subprocess
 import json
 
+#variables
+org = 771
+acu = 34342
+
 # Run the external script
 result = subprocess.run(['op', 'core', 'describe-acu', '--options', 'withShadows', '771', '34342'], capture_output=True, text=True)
 
-#print(result.stdout)
+
 
 # Parse the output string as JSON
 try:
@@ -14,12 +18,20 @@ try:
     # Extract the specific field (e.g., "person")
     expiration_date = output_data.get("shadow", {}).get("state", {}).get("reported", {}).get("status", {}).get("httpCertificate", {}).get("notAfter", {})
     percent_life_remaining = output_data.get("shadow", {}).get("state", {}).get("reported", {}).get("status", {}).get("httpCertificate", {}).get("percentLifeRemaining", {})
+
     # Convert the data to a JSON-formatted string
     #json_data = json.dumps(specific_field, indent=2)
 
     #print(output_data)
     print("HTTP Certificate Experiation Date is",expiration_date)
-    print("Percent of Certificat Life Remaining",percent_life_remaining)
+    print("Percent of Certificate Life Remaining",percent_life_remaining)
+
+
+    #if statement to detect if percent life remaining of the certificate is less then 15%
+    if int(percent_life_remaining) < int(15):
+        print("Certificate close to expiring")
+    else:
+        print("Certificate is in a good state")
 
 
 except json.JSONDecodeError as e:
